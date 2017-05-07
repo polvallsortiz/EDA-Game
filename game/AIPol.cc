33b72692c9bfa3a19aca170fb523b5a29bd1b051
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-05-08 01:04:03 +0200

    Work to start the idea of a continuous DFS from every position

--- game/AIPol.cc
+++ game/AIPol.cc
@@ -1,7 +1,8 @@
 #include "Player.hh"
 #include <queue>
 #include <cmath>
-
+#include <iostream>
+#include <list>
 using namespace std;
 
 /**
@@ -20,6 +21,33 @@ struct PLAYER_NAME : public Player {
     static Player* factory () {
         return new PLAYER_NAME;
     }
+
+    /**
+     * DFS ALG from EDA code pdf
+     * @param G
+     * @return The list of Deep First Search from the vertex 0
+     */
+    list<int> dfs_ite (const graph& G) {
+        int n = G.size();
+        list<int> L;
+        stack<int> S;
+        vector<boolean> vis(n, false);
+        for (int u = 0; u < n; ++u) {
+            S.push(u);
+            while (not S.empty()) {
+                int v = S.top();  S.pop();
+                if (not vis[v]) {
+                    vis[v] = true;
+                    L.push_back(v);
+                    for (int w : G[v]) {
+                        S.push(w);
+                    }
+                }
+            }
+        }
+        return L;
+    }
+
     /**
      * FUNCTIONS AND ALGORITHMS FOR THE PRINCIPAL FUNCTIONS
      */
@@ -36,7 +64,7 @@ struct PLAYER_NAME : public Player {
             for(int j = 0; j < actual_neighbours.size(); ++j) {
                 if(bv[i] == actual_neighbours[j]) return bv[i];
             }
-        }
+         }
         return -1;
     }
     void empty_neighbours_funtion(const vector<int>& neighbours, vector<int>& empty_neighbours) {
@@ -47,23 +75,53 @@ struct PLAYER_NAME : public Player {
             }
         }
     }
+    void movement_with_turbo(const vector<int>& empty, Movement& movement) {
+        cerr << "TURBO MOVEMENT" << endl;
+        movement.use_bonus = true;
+    }
+    void movement_with_ghost(const vector<int>& neighbours, Movement& movement) {
+        cerr << "GHOST MOVEMENT" << endl;
+        movement.use_bonus = true;
+    }
+    /**
+     * Function to prove if the next vertex has empty neighbours
+     * @param vertex1
+     * @return True if has neighbours, False if not
+     */
+    bool next_next(int vertex_id) {
+        Vertex vertex1 = vertex(vertex_id);
+        vector<int> neighbours = vertex1.neighbours;
+        vector<int> empty_neighbours;
+        empty_neighbours_funtion(neighbours,empty_neighbours);
+        return !empty_neighbours.empty();
+    }
 
+    bool in_positions(int vertex_id) {
+        int i = 0;
+        bool found = false;
+        while(i < bike_positions.size() and not found) {
+            if(bike_positions[i] == vertex_id) found = true;
+            ++i;
+        }
+        return found;
+    }
     /**
      * Attributes for your player can be defined here.
      */
-    //vector<int> my_awesome_vector_of_integers;
+    vector<int> bike_positions;
 
    /**
     * to_bonus function, try to go to bonus if is neigh
     * @param bike_id
     */
     void to_bonus(const Bike& my_bike) {
+       cerr << "to_bonus function" << endl;
        Movement movement(my_bike.id);
        int actual_vertex_bike_id = my_bike.vertex;
        vector<int> neighbours = vertex(actual_vertex_bike_id).neighbours;
        vector<int> empty_neighbours;
        empty_neighbours_funtion(neighbours,empty_neighbours);
-       //ESTEM ABANS DE DEIXAR ANAR EL BONUS
+       //ESTEM DESPRES DE DEIXAR ANAR EL BONUS
         if(round() >= bonus_round()) {
             //INTENTEM ANAR AL BONUS SI ÉS POSSIBLE
             int possible = vertex_bonus_neighbour(actual_vertex_bike_id);
@@ -72,15 +130,36 @@ struct PLAYER_NAME : public Player {
                 movement.use_bonus = false;
             }
             else {
-                movement.next_vertex = empty_neighbours[1];
-                if(my_bike.bonus != None) movement.use_bonus = true;
-                else movement.use_bonus = false;
+                if(my_bike.bonus == Turbo) movement_with_turbo(empty_neighbours,movement);
+                else if(my_bike.bonus == Ghost) movement_with_ghost(neighbours,movement);
+
+                    if(!empty_neighbours.empty()) {
+                        int i = 0;
+                        bool found = false;
+                        while(i < empty_neighbours.size() and not found) {
+                            if(next_next(empty_neighbours[i]) and vertex(empty_neighbours[i]).wall == -1 and not in_positions(empty_neighbours[i])) found = true;
+                            else ++i;
+                        }
+                        movement.next_vertex = empty_neighbours[i];
+                    }
+                    else movement.next_vertex = neighbours[0];
+                movement.use_bonus = false;
             }
         }
         else {
-            movement.next_vertex = empty_neighbours[1];
+            if(!empty_neighbours.empty()) {
+                int i = 0;
+                bool found = false;
+                while(i < empty_neighbours.size() and not found) {
+                    if(next_next(empty_neighbours[i]) and vertex(empty_neighbours[i]).wall == -1 and not in_positions(empty_neighbours[i])) found = true;
+                    else ++i;
+                }
+                movement.next_vertex = empty_neighbours[i];
+            }
+            else movement.next_vertex = neighbours[0];
             movement.use_bonus = false;
         }
+       bike_positions.push_back(movement.next_vertex);
        command(movement);
     }
 
